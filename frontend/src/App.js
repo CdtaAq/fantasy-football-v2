718fdaf353edaa7e2cabc8ad1504931357a4b30e
chris Taylor <ctay456a@gmail.com>
2025-09-14 00:24:53 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import LeaguePage from "./pages/LeaguePage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/leagues/:id" element={<LeaguePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
