71780887e66e3ac37e3b781f980d01be0f723dd9
chris Taylor <ctay456a@gmail.com>
2025-09-14 00:23:35 -0400

    Create authController.js

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,38 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user.id)
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user.id)
+    });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  res.json(req.user);
+};
